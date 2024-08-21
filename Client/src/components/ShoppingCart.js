bd31ccdc2e3597aa66d6edf70533024f17773dda
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-08-21 20:32:14 +0300

    Update ShoppingCart.js

--- Client/src/components/ShoppingCart.js
+++ Client/src/components/ShoppingCart.js
@@ -7,6 +7,7 @@ const ShoppingCart = ({
   addToCart,
   removeFromCart,
   deleteFromCart,
+  changeQuantity,
   toggleCart,
 }) => {
   // Calculate the total price of items in the cart
@@ -19,11 +20,10 @@ const ShoppingCart = ({
 
   // Function to proceed to the checkout page
   const proceedToCheckout = () => {
-    //Check if there are any items with a quantity of 0
+    // Check if there are any items with a quantity of 0
     const hasZeroQuantityItem = cart.some((item) => item.quantity === 0);
     if (cart.length < 1) {
-      //  Check if any item in the cart has a quantity of 0
-      alert("Cart is Empty , please add at least one product.");
+      alert("Cart is Empty, please add at least one product.");
       return;
     }
     if (hasZeroQuantityItem) {
@@ -37,14 +37,12 @@ const ShoppingCart = ({
     navigate("/order");
   };
 
-  // Function to handle adding an item to the cart
-  const handleAddToCart = (item) => {
-    addToCart(item);
-  };
-
-  // Function to handle removing an item from the cart
-  const handleRemoveFromCart = (item) => {
-    removeFromCart(item);
+  // Function to handle updating the quantity of an item in the cart
+  const handleQuantityChange = (item, quantity) => {
+    // Ensure quantity is a positive number
+    if (quantity < 0) return;
+    const updatedItem = { ...item, quantity };
+    changeQuantity(updatedItem, quantity);
   };
 
   // Function to handle deleting an item from the cart
@@ -67,13 +65,30 @@ const ShoppingCart = ({
               {item.name}
               <br />
             </span>
-            <span> Quantity: {item.quantity} , </span>
-            <span> Price for one: {item.price} NIS</span>
+            <span>
+              Quantity:
+              <input
+                type="number"
+                value={item.quantity}
+                onChange={(e) =>
+                  handleQuantityChange(item, Number(e.target.value))
+                }
+                min="0"
+              />
+            </span>
+            <span>Units Price for one: {item.price} NIS</span>
             <br />
             <span> Total Price: {item.price * item.quantity} NIS</span>
             <div>
-              <button onClick={() => handleAddToCart(item)}>+</button>
-              <button onClick={() => handleRemoveFromCart(item)}>-</button>
+              <button className="plus-button" onClick={() => addToCart(item)}>
+                +
+              </button>
+              <button
+                className="minus-button"
+                onClick={() => removeFromCart(item)}
+              >
+                -
+              </button>
               <button onClick={() => handleDeleteFromCart(item._id)}>
                 Remove
               </button>
@@ -81,7 +96,7 @@ const ShoppingCart = ({
           </li>
         ))}
       </ul>
-      <h3>Total: {totalPrice} NIS</h3>
+      <h3>Total price of your cart: {totalPrice} NIS</h3>
       <button className="button" onClick={proceedToCheckout}>
         Checkout
       </button>
