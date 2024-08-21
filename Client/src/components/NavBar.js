ec339a7910da7ac2c78c204b9c51a0ea370d66da
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-08-21 20:30:47 +0300

    Update NavBar.js

--- Client/src/components/NavBar.js
+++ Client/src/components/NavBar.js
@@ -6,13 +6,21 @@ import { FaShoppingCart } from "react-icons/fa";
 
 const NavBar = ({ toggleCart, cartQuantity, totalPrice }) => {
   return (
-    <nav>
-      {/* Logo for the shoe store */}
-      <div className="logo"> "Lech-lecha" Shoe Store </div>
+    <nav className="nav-bar">
+      <li>
+        {/* Special Text Container */}
+        <div className="special-container">Our Quality Your Plesure </div>
+      </li>
+      <li>
+        {/* Logo for the shoe store */}
+        <div className="logo"> "Lech-lecha" Shoe Store </div>
+      </li>
       <ul>
         {/* Navigation link to the Home Page */}
         <li>
-          <a href="/"> Home Page </a>
+          <a className="special-container" href="/">
+            Home Page
+          </a>
         </li>
         {/* Cart icon with quantity and total price */}
         <li className="cart-icon" onClick={toggleCart}>
